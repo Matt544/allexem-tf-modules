5cc1147cb3d656668eddd7a58aa5f0d184195343
Matt544 <joe_coolmt@hotmail.com>
2025-05-23 09:50:03 -0600

    Automated message for v0.0.16

--- modules/s3-backend/main.tf
+++ modules/s3-backend/main.tf
@@ -13,9 +13,9 @@ terraform {
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
 }
 
 resource "aws_s3_bucket_versioning" "enabled" {  # do I actually want versioning?
