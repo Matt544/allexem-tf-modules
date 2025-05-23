0c29f01e739d609bf8767431cbf59d9ac9f05009
Matt544 <joe_coolmt@hotmail.com>
2025-05-23 10:13:53 -0600

    Automated message for v0.0.20

--- modules/s3-secrets/main.tf
+++ modules/s3-secrets/main.tf
@@ -14,8 +14,7 @@ resource "aws_s3_bucket" "secrets_bucket" {
   bucket = var.bucket_name
 
   lifecycle {
-    # prevent_destroy = true
-    prevent_destroy = false
+    prevent_destroy = true
   }
 }
 
